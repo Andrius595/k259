96fdf40cd8ee7b9c7e9fc3e7d037bd4f40e2199c
Martynas Tvaska <73237433+Marcynas@users.noreply.github.com>
2023-03-07 14:22:08 +0200

    frontas

--- frontend/pages/dashboard.vue
+++ frontend/pages/dashboard.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import {useUserStore} from "~/stores/userStore";
+import { useUserStore } from "~/stores/userStore";
 
-definePageMeta({middleware: ['auth']})
+definePageMeta({ middleware: ["auth"] });
 
-const userStore = useUserStore()
+const userStore = useUserStore();
 
-const user = userStore.getUser
+const user = userStore.getUser;
 </script>
 
 <template>
@@ -24,10 +24,24 @@ const user = userStore.getUser
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
           <div class="p-6 bg-white border-b border-gray-200">
-            {{ user.first_name }}, you're logged in!
+            Welcome,
+            {{ user?.first_name }}, you're logged in!
+          </div>
+        </div>
+      </div> 
+    </div>
+
+         <!-- create litter button /litter/create.vue-->
+         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+          <div class="p-6 bg-white border-b border-gray-200">
+            <a href="/litter/create">
+              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Create Litter
+              </button>
+            </a>
           </div>
         </div>
       </div>
-    </div>
   </NuxtLayout>
 </template>
