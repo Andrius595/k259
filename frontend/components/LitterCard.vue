8a6b75cc82844293ec0b38c81a509c18515c17a3
Martynas Tvaska <73237433+Marcynas@users.noreply.github.com>
2023-03-13 15:06:26 +0200

    litter list front :)

--- frontend/components/LitterCard.vue
+++ frontend/components/LitterCard.vue
@@ -1,23 +1,66 @@
+// Purpose: Display litter information in a card
+//
+// Props:
+//   litter: Litter object
+//
+// Usage:
+//   <LitterCard :litter="litter" />
+
+//Property 'litter' does not exist on type
+
+<script setup lang="ts">
+import { Litter } from '~~/types/litterTypes';
+
+// props
+const litter = defineProps<{ litter: Litter }>();
+
+// methods
+const navigateToLitter = () => {
+  navigateTo(`/litter/edit/${litter.litter.id}`);
+};
+
+
+</script>
+
 <template>
+  <!-- card -->
+  <div
+    class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
+  >
+    <!-- litter image from provided litter image_path location -->
+    <div class="flex justify-center">
+      <img
+        class="w-32 h-32 object-cover"
+        :src="litter.litter.image_path || 'https://via.placeholder.com/150'"
+        alt="litter image"
+      />
 
-    <!-- card -->
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-      <!-- image -->
-      <div class="flex justify-center">
-        <slot name="image"></slot>
-      </div>
-      <!-- title -->
-      <div class="mt-4">
-        <slot name="title"></slot>
-      </div>
-      <!-- description -->
-      <div class="mt-4">
-        <slot name="description"></slot>
-      </div>
-        <!-- button --> 
-        <div class="mt-4">
-        <slot name="button"></slot>
-        </div>  
-        </div>
-
+    </div>
+    <!-- title -->
+    <div class="mt-4">
+      <h1 class="text-2xl font-bold text-gray-900">{{ litter.litter.id }}</h1>
+    </div>
+    <!-- description -->
+    <div class="mt-4">
+      <p class="text-gray-600">{{ litter.litter.description }}</p>
+    </div>
+    <!-- date -->
+    <div class="mt-4">
+      <p class="text-gray-600">{{ litter.litter.created_at }}</p>
+    </div>
+    <!-- size -->
+    <div class="mt-4">
+      <p class="text-gray-600">{{ litter.litter.size }}</p>
+    </div>
+    <!-- button -->
+    <div class="mt-4">
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        @click="navigateToLitter"
+      >
+        Edit
+      
+      </button>
+    </div>
+  </div>
 </template>
