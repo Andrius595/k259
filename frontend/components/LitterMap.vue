6695c0848c888618971fd0b84d936328c9e726da
Andrius <andrius.cereska@ktu.edu>
2023-03-13 21:10:20 +0200

    Leaflet map example

---
+++ frontend/components/LitterMap.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="map-component">
+    <client-only>
+      <l-map :useGlobalLeaflet="false" :zoom="zoom" :center="center">
+        <l-tile-layer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            layer-type="base"
+            name="OpenStreetMap"
+        />
+        <template v-for="litterListCoordinate in litterListCoordinates">
+          <l-marker :lat-lng="litterListCoordinate.geometry.coordinates" :radius="5" :options="{color: 'red'}">
+            <l-popup>
+              <div class="flex flex-col gap-1">
+                <h3 class="text-xl font-bold">Litter</h3>
+                <div class="flex flex-row">
+                  <div class="font-bold">Description:</div>
+                  <div class="ml-2">{{litterListCoordinate.properties.description}}</div>
+                </div>
+                <div class="flex flex-row">
+                  <div class="font-bold">ID:</div>
+                  <div class="ml-2">{{litterListCoordinate.properties.id}}</div>
+                </div>
+                <button class="bg-blue-500 hover:bg-blue-700 transition font-bold py-2 px-4 rounded mt-2">
+                  <NuxtLink :href="`/litter/edit/${litterListCoordinate.properties.id}`"><span class="text-white">Edit litter</span></NuxtLink>
+                </button>
+              </div>
+            </l-popup>
+          </l-marker>
+        </template>
+      </l-map>
+    </client-only>
+  </div>
+</template>
+
+<script setup lang="ts">
+import "leaflet/dist/leaflet.css";
+import {LMap, LTileLayer, LMarker, LPopup} from "@vue-leaflet/vue-leaflet";
+import {Litter} from "~/types/litterTypes";
+
+const zoom = ref(7);
+const center = ref({lat: 55.2, lon: 24});
+
+const LittersList = ref<Litter[]>([])
+await loadLitters()
+
+const litterListCoordinates = computed(() => {
+  return LittersList.value.map((litter) => {
+    return {
+      type: "Feature",
+      properties: {
+        description: litter.description,
+        id: litter.id,
+      },
+      geometry: {
+        type: "Point",
+        coordinates: { lon: litter.longitude, lat: litter.latitude },
+      },
+    };
+  });
+})
+
+async function loadLitters() {
+  const response = await $fetch('/api/litter')
+  if (response.status) {
+    LittersList.value = response.data
+  }
+}
+</script>
---
+++ frontend/pages/map/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <NuxtLayout name="auth-layout">
+    <template #header>Map</template>
+
+    <LitterMap style="height: 600px" />
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+import LitterMap from '~/components/LitterMap.vue'
+definePageMeta(['auth'])
+</script>
+
+<style scoped>
+</style>
