194dd81b4f636bf539306db6289e95efd1b43c9e
Jakobinas <100943594+Jakobinas@users.noreply.github.com>
2023-03-14 17:22:08 +0000

    lt zemelpais

--- frontend/pages/dashboard.vue
+++ frontend/pages/dashboard.vue
@@ -37,7 +37,7 @@ const user = userStore.getUser;
       <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 bg-white border-b border-gray-200">
           <h2 class="font-semibold text-xl text-gray-800 leading-tight m-2">
-            Active litter map 📍
+            Šiukšlių žemėlapis 📍
           </h2>
           <LitterMap style="height: 500px" />
         </div>
