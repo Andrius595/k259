7d06d5c06a6e4fe1625a5b9b072026367d0466a1
Andrius <andrius.cereska@ktu.edu>
2023-03-28 18:09:37 +0300

    Added ability to join events.

--- backend/app/Models/Event.php
+++ backend/app/Models/Event.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Event extends Model
@@ -23,8 +24,28 @@ class Event extends Model
         'has_ended',
     ];
 
+    protected $appends = [
+        'has_user_joined',
+        'joined_users_count',
+    ];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function joinedUsers(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function getHasUserJoinedAttribute(): bool
+    {
+        return $this->joinedUsers()->where('user_id', auth()->id())->exists();
+    }
+
+    public function getJoinedUsersCountAttribute(): int
+    {
+        return $this->joinedUsers()->count();
     }
 }
