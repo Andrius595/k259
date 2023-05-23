2bdb42a122a0d2574fe734ef5b4fa6dcb19f45e5
Andrius <andrius.cereska@ktu.edu>
2023-05-23 21:00:28 +0300

    Fixed invalid end tag

--- frontend/components/PrizeCard.vue
+++ frontend/components/PrizeCard.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { Prize } from '~~/types/prizeTypes';
-import {DateTime} from 'luxon'
+import {Prize} from '~~/types/prizeTypes';
 import {useUserStore} from "~/stores/userStore";
 import {AllPermissions} from "~/enums/permissions";
 import {AllRoles} from "~/enums/roles";
@@ -26,32 +25,31 @@ const navigateToPrize = () => {
 <template>
   <!-- card -->
   <div
-    class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
+      class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
   >
-    </div>
-    <!-- title -->
-    <div class="mt-4">
-      <h1 class="text-2xl font-bold text-gray-900">Prizas: </h1>
-      <h1 class="text-2xl text-gray-900">{{ gift.name }}</h1>
-    </div>
-    <!-- description -->
-    <div class="mt-4">
-      Aprašymas:
-      <p class="text-gray-600">{{ prize.description }}</p>
-    </div>
-    <!-- size -->
-    <div class="mt-4">
-      <p class="text-gray-600">Kaina: {{ prize.price }}</p>
-    </div>
-    <!-- button -->
-    <div class="mt-4">
-      <button
+  </div>
+  <!-- title -->
+  <div class="mt-4">
+    <h1 class="text-2xl font-bold text-gray-900">Prizas: </h1>
+    <h1 class="text-2xl text-gray-900">{{ gift.name }}</h1>
+  </div>
+  <!-- description -->
+  <div class="mt-4">
+    Aprašymas:
+    <p class="text-gray-600">{{ prize.description }}</p>
+  </div>
+  <!-- size -->
+  <div class="mt-4">
+    <p class="text-gray-600">Kaina: {{ prize.price }}</p>
+  </div>
+  <!-- button -->
+  <div class="mt-4">
+    <button
         v-if="canEditPrize"
         class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         @click="navigateToEvent"
-      >
-        Redaguoti
+    >
+      Redaguoti
     </button>
-    </div>
   </div>
 </template>
--- frontend/pages/event/list.vue
+++ frontend/pages/event/list.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Event } from '~~/types/eventTypes';
+import {Event} from '~~/types/eventTypes';
 
 import EventCard from '~/components/EventCard.vue';
 import {useUserStore} from "~/stores/userStore";
@@ -14,9 +14,9 @@ const canCreateEvent = computed(() => {
 })
 
 async function loadLitters() {
-  const response :any = await $fetch('/api/event')
+  const response: any = await $fetch('/api/event')
   if (response.status) {
-    EventsList.value = response.data 
+    EventsList.value = response.data
   }
 }
 
@@ -37,18 +37,18 @@ async function loadLitters() {
     <div class="my-12">
       <!-- grid filled with <LitterCard> -->
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        
+
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-          
+
           <div class="p-6 bg-white border-b border-gray-200">
             <a v-if="canCreateEvent" href="/event/create" class="pr-8">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Sukurti naują renginį
-        </button>
-      </a>
+              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Sukurti naują renginį
+              </button>
+            </a>
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
               <!--once load Litters is finished display litter card-->
-              <EventCard v-for="event in EventsList" :key="event.id" :event="event" />
+              <EventCard v-for="event in EventsList" :key="event.id" :event="event"/>
             </div>
           </div>
         </div>
